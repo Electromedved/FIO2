a2022287ebccbf296d35f001168cd5803ac2994d
Виталий Анатольевич <pigcontrol@gmail.com>
2020-06-14 19:59:55 +0300

    Add project files.

---
+++ FIO2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIO2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fName;
+            string lName;
+            string pogonyalo;
+            int height;
+            int weight;
+            string mobile;
+            int age;
+            string town;
+            string street;
+            int house;
+            int apartment;
+
+            Console.WriteLine("Ваше имя:");
+            Console.ReadLine(fName);
+            Console.WriteLine("Ваша фамилия:");
+            Console.ReadLine(lName);
+            Console.WriteLine("Ваше отчество:");
+            Console.ReadLine(pogonyalo);
+            Console.WriteLine("Ваш рост:");
+            Console.ReadLine(height);
+            Console.WriteLine("Ваш вес:");
+            Console.ReadLine(weight);
+        }
+    }
+}
